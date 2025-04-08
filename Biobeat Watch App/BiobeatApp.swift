0432f458757845d73eb554b5db4639178b7c037c
Darien Moment <dmoment18@gmail.com>
2025-04-08 00:24:31 -0400

    Initial Commit

---
+++ Biobeat Watch App/BiobeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiobeatApp.swift
+//  Biobeat Watch App
+//
+//  Created by Darien on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Biobeat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
